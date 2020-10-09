bf14e7d41c00219f9e029978ce02c3f45fe0c517
Edith Puclla <epuclla@e1z3r7p7.42.us.org>
2020-10-09 16:56:02 -0700

    reading Map

--- mac/src/main.c
+++ mac/src/main.c
@@ -1,14 +1,49 @@
 #include	"../minilibx/mlx.h"
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
-int main()
+int main(int argc, char **argv)
 {
 
-  void	*mlx_ptr;
-  void 	*win_ptr;
+  //void	*mlx_ptr;
+  //void 	*win_ptr;
 
-  mlx_ptr = mlx_init();
+  //for read the file
+  char  *line;
+  int fd;
+  int ret;
+  //mlx_ptr = mlx_init();
+
+  // Validating the Map argc = 2 and argv = 1
+  // validaten --save argv = 2
+
+  ret = 0;
+  if (argc == 2)
+  {
+    fd = open(argv[1],O_RDONLY);
+    while((ret = get_next_line(fd, &line)) > 0)
+    {
+      printf("%s\n", line);
+      free(line);
+    }
+    if (ret == -1)
+      printf("%s", "I can't read your FD");
+    else if(ret == 0)
+    {
+      free(line);
+    }
+    close(fd);
+
+    //win_ptr = mlx_new_window(mlx_ptr,500, 500, "mlx 42" );
+    //mlx_loop(mlx_ptr);
+
+    
+  }
   
-  win_ptr = mlx_new_window(mlx_ptr,500, 500, "mlx 42" );
-  mlx_loop(mlx_ptr);
+  if (argc == 1)
+    printf("%s", "Error map don't sending");
+  
+  
 
 }
